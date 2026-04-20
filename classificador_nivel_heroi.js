2d9dae9059fa18ebf098a495d3c70ed68167e6cd
adrianoanthonymma16-boop <adrianoanthonymma16@gmail.com>
2026-04-20 19:26:56 -0300

    first commit

---
+++ classificador_nivel_heroi.js
@@ -0,0 +1,29 @@
+let nomeHeroi = prompt("Digite o nome do herói:");
+let nivelHeroi = 5000;
+let classificacaoHeroi = ""
+
+switch (true) {
+    case (nivelHeroi <= 1000):
+        classificacaoHeroi = "Ferro";
+        break;
+    case (nivelHeroi >= 1001 && nivelHeroi <= 2000):
+        classificacaoHeroi = "Bronze";
+        break;
+    case (nivelHeroi >= 2001 && nivelHeroi <= 5000):
+        classificacaoHeroi = "Prata";
+        break;
+    case (nivelHeroi >= 5001 && nivelHeroi <= 8000):
+        classificacaoHeroi = "Platina diamante";
+        break;
+    case (nivelHeroi >= 8001 && nivelHeroi <= 9000):
+        classificacaoHeroi = "Ascendente";
+        break;
+    case (nivelHeroi >= 9001 && nivelHeroi <= 10000):
+        classificacaoHeroi = "Imotal";
+        break;
+    case (nivelHeroi >= 10001):
+        classificacaoHeroi = "Radiante";
+        break;
+}
+
+console.log("O heroi de nome " + nomeHeroi + "Tem como classificação o nível: " + classificacaoHeroi)
